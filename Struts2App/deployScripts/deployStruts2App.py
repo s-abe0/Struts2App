13597d9e68cf5284cf2a3d2b9c9c2defa5ee5095
ShaneAbe <ShaneAbe@9.80.220.214>
2020-07-17 10:44:30 -0400

    update deploy script

--- Struts2App/deployScripts/deployStruts2App.py
+++ Struts2App/deployScripts/deployStruts2App.py
@@ -1,9 +1,17 @@
 deployEAR="/opt/IBM/WebSphere/AppServer/earFiles/Struts2App.ear"
+appManager="AdminControl.queryNames('cell=nullNode01Cell,node=wasNode01,type=ApplicationManager,process=server1,*')
 
 # Install the app
-#AdminApp.install(deployEAR, "[-appname Struts2AppEAR]")
-#AdminConfig.save()
+def installStruts2App:
+    AdminApp.install(deployEAR, "[-appname Struts2AppEAR]")
+    AdminConfig.save()
+    
+    AdminControl.invoke(appManager, 'startApplication','Struts2AppEAR')
 
 # Update the app
-AdminApp.update('Struts2AppEAR', 'app', '[-operation update -contents /opt/IBM/WebSphere/AppServer/earFiles/Struts2App.ear -usedefaultbindings -nodeployejb]')
-AdminConfig.save()
+def updateStruts2App:
+    AdminApp.update('Struts2AppEAR', 'app', '[-operation update -contents /opt/IBM/WebSphere/AppServer/earFiles/Struts2App.ear -usedefaultbindings -nodeployejb]')
+    AdminConfig.save()i
+
+installStruts2App()
+updateStruts2App()
